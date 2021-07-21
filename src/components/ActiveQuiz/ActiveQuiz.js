d226e0f96dd9997f676d0846f60fb2ae8301de0f
Denis <denis.bortnikov17@gmail.com>
2021-07-21 16:14:48 +0300

    [DCYB-48] Create AnswersList and AnswerItem

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,23 +1,21 @@
 import React from 'react'
+import AnswersList from './AnswersList/AnswersList'
 import classes from './ActiveQuiz.module.css'
 
 const ActiveQuiz = props => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <stong>2.</stong>&nbsp;
+        <strong>2.</strong>&nbsp;
         Как дела?
       </span>
 
       <small>4 из 12</small>
     </p>
 
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
+    <AnswersList 
+      answers={props.answers}
+    />
   </div>
 ) 
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,7 +4,16 @@ import classes from './Quiz.module.css'
 
 class Quiz extends Component {
   state = {
-    quiz: []
+    quiz: [
+      {
+        answers: [
+          {text: 'Вопрос 1'},
+          {text: 'Вопрос 2'},
+          {text: 'Вопрос 3'},
+          {text: 'Вопрос 4'}
+        ]
+      }
+    ]
   }
 
 
@@ -12,8 +21,8 @@ class Quiz extends Component {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz/>
+          <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz answers={this.state.quiz[0].answers}/>
         </div>
       </div>
     )
