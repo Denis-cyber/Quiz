ce3bfaa5abbff982ad50115b02d15a8352bfdd35
Denis <denis.bortnikov17@gmail.com>
2021-07-29 16:47:39 +0300

    [DCYB-78] Form validation (buttons Log in and Sign up)

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -11,6 +11,7 @@ function validateEmail(email) {
 
 export default class Auth extends Component {
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -77,8 +78,14 @@ export default class Auth extends Component {
 
     formControls[controlName] = control;
 
+    let isFormValid = true;
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+
     this.setState({
       formControls,
+      isFormValid,
     });
   };
 
@@ -110,11 +117,19 @@ export default class Auth extends Component {
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
             {this.renderInputs()}
 
-            <Button type="success" onClick={this.loginHandler}>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               Войти
             </Button>
 
-            <Button type="primary" onClick={this.registerHandler}>
+            <Button
+              type="primary"
+              disabled={!this.state.isFormValid}
+              onClick={this.registerHandler}
+            >
               Зарегистрироваться
             </Button>
           </form>
